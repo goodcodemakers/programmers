1018899c5ff080e40fb7740200e9781b637a6924
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-03-16 22:31:15 +0900

    Delete 최댓값만들기(1).js

--- 최댓값만들기(1).js
+++
@@ -1,8 +0,0 @@
-function solution(numbers) {
-    numbers.sort((a, b) => {
-       return a - b;
-     });
-     var answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-   return answer;
-}
-
