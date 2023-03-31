df32e50e8fdb9689624087b327ae441b75a16b92
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-03-31 10:26:22 +0900

    new 0 step

--- 0단계/n의배수고르기.js
+++ 0단계/n의배수고르기.js
@@ -1,4 +1,11 @@
 function solution(n, numlist) {
     var answer = [];
+    for (let i = 0; i < numlist.length; i++) {
+        if(numlist[i] % n == 0){
+            answer.push(numlist[i])
+        }
+        console.log(answer);
+    }
     return answer;
 }
+solution(3,[4, 5, 6, 7, 8, 9, 10, 11, 12])
