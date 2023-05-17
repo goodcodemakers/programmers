e8c1a1aca5c0843bcaef49e4c90dad579d62e03c
choi jeong ho <113665732+goodcodemakers@users.noreply.github.com>
2023-05-17 10:08:57 +0900

    new solution

---
+++ 0단계/문자열의앞의n글자.js
@@ -0,0 +1,8 @@
+function solution(my_string, n) {
+    var answer = '';
+    answer = my_string.slice(0,n);
+    return answer;
+}
+
+//slice();는 처음오는변수는 어디서 시작할건지  
+// n에 들어갈것은 끝나는지점을 말해주는 함수입니다.
--- 0단계/소문자로바꾸기.js
+++ 0단계/소문자로바꾸기.js
@@ -4,4 +4,4 @@ function solution(myString) {
     return answer;
 }
 
-//소문자로 바꿔주는 메소드만 알면 쉽게 풀 수 있는 문제입니다.
+//소문자로 바꿔주는 함수만 알면 쉽게 풀 수 있는 문제입니다.  
